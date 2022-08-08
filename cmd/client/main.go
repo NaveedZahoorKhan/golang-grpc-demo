409e753b4e18fe210f11a6dc6f8eb446a8029a2d
naveed zahoor <zahoorn@theentertainerasia.com>
2022-08-08 21:03:33 +0500

    first commit

---
+++ cmd/client/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"example.com/grpc/pb"
+	"example.com/grpc/sample"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func main() {
+	serverAddress := flag.String("address", "", "the server address")
+
+	flag.Parse()
+
+	log.Printf("dial server %s", *serverAddress)
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+
+	laptop := sample.NewLaptop()
+
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Printf("laptop already exist")
+		} else {
+			log.Fatal(err)
+		}
+	}
+	log.Printf("laptop created: %s", res.Id)
+
+	findLaptopReq := &pb.FindLaptopRequest{
+		Id: res.Id,
+	}
+
+	findLaptopRes, err1 := laptopClient.FindLaptop(context.Background(), findLaptopReq)
+
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	log.Printf("found laptop with id %s", findLaptopRes.GetLaptop())
+
+}
---
+++ service/laptop_server.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"example.com/grpc/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type LaptopServer struct {
+	Store LaptopStore
+}
+
+// FindLaptop implements pb.LaptopServiceServer
+func (server *LaptopServer) FindLaptop(ctx context.Context, req *pb.FindLaptopRequest) (*pb.FindLaptopResponse, error) {
+	laptopId := req.Id
+	log.Printf("Received request to find laptop with id: %s", laptopId)
+
+	if len(laptopId) > 0 {
+		_, err := uuid.Parse(laptopId)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop ID is not a valid UUID: %v", err)
+		}
+	}
+	laptop, err := server.Store.Find(laptopId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.FindLaptopResponse{
+		Laptop: laptop,
+	}
+	return resp, nil
+}
+
+func NewLaptopServer(store LaptopStore) *LaptopServer {
+	return &LaptopServer{
+		Store: store,
+	}
+}
+
+func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
+	laptop := req.GetLaptop()
+	log.Printf("Received request to create laptop: %v", laptop)
+
+	if len(laptop.Id) > 0 {
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop ID is not a valid UUID: %v", err)
+		}
+	} else {
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return nil, err
+		}
+		laptop.Id = id.String()
+	}
+
+	err := server.Store.Save(laptop)
+	if err != nil {
+		code := codes.Internal
+		if errors.Is(err, ErrAlreadyExist) {
+			code = codes.AlreadyExists
+		}
+		return nil, status.Errorf(code, "failed to save laptop")
+	}
+	log.Printf("saved laptop with id: %s", laptop.Id)
+
+	res := &pb.CreateLaptopResponse{
+		Id: laptop.Id,
+	}
+
+	return res, nil
+
+}
